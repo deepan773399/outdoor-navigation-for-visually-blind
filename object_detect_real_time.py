905e267a5e3532bdd91548b3e95b26034000a123
deepan773399 <54079477+deepan773399@users.noreply.github.com>
2020-12-13 23:09:44 +0530

    Update and rename darknet_train to object_detect_real_time.py

---
+++ object_detect_real_time.py
@@ -0,0 +1,3 @@
+#For model development we use darknet and train our model by using labelled data after do labling on the data
+#after that a weight file is generated
+#and by using that weight file and yolo.cfg file we detect the object in real by the object_detect_real_time.py file
